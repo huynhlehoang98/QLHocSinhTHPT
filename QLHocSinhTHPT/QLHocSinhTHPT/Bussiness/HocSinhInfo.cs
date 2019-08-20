ecd6e5273e622e5517b9a7ba82fe8c49e25495bc
lehoanghuynh <43942549+lehoanghuynh@users.noreply.github.com>
2019-08-20 22:47:58 +0700

    Update HocSinhInfo.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/HocSinhInfo.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/HocSinhInfo.cs
@@ -8,5 +8,37 @@ namespace QLHocSinhTHPT.Bussiness
 {
     class HocSinhInfo
     {
+        public HocSinhInfo()
+        {
+
+        }
+
+        private String m_MaHocSinh;
+        public String MaHocSinh
+        {
+            get { return m_MaHocSinh; }
+            set { m_MaHocSinh = value; }
+        }
+
+        private String m_HoTen;
+        public String HoTen
+        {
+            get { return m_HoTen; }
+            set { m_HoTen = value; }
+        }
+
+        private bool m_GioiTinh;
+        public bool GioiTinh
+        {
+            get { return m_GioiTinh; }
+            set { m_GioiTinh = value; }
+        }
+
+        private DateTime m_NgaySinh;
+        public DateTime NgaySinh
+        {
+            get { return m_NgaySinh; }
+            set { m_NgaySinh = value; }
+        }
     }
 }
