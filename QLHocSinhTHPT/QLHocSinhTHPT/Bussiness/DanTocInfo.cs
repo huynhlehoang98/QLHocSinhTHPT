fd807ca0e4e2aed99da742b3841dd09aca9f1083
lehoanghuynh <43942549+lehoanghuynh@users.noreply.github.com>
2019-08-20 22:47:01 +0700

    Update DanTocInfo.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/DanTocInfo.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/DanTocInfo.cs
@@ -6,5 +6,23 @@ namespace QLHocSinhTHPT.Bussiness
 {
     class DanTocInfo
     {
+        public DanTocInfo()
+        {
+
+        }
+
+        private String m_MaDanToc;
+        public String MaDanToc
+        {
+            get { return m_MaDanToc; }
+            set { m_MaDanToc = value; }
+        }
+
+        private String m_TenDanToc;
+        public String TenDanToc
+        {
+            get { return m_TenDanToc; }
+            set { m_TenDanToc = value; }
+        }
     }
 }
