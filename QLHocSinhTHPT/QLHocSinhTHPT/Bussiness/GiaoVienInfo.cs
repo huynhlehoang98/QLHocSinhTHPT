376276397c242d61f92242bc14be996bb102e89b
lehoanghuynh <43942549+lehoanghuynh@users.noreply.github.com>
2019-08-20 22:47:38 +0700

    Update GiaoVienInfo.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/GiaoVienInfo.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/GiaoVienInfo.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace QLHocSinhTHPT.Bussiness
 {
     class GiaoVienInfo
     {
+        public GiaoVienInfo()
+        {
+
+        }
+
+        private String m_MaGiaoVien;
+        public String MaGiaoVien
+        {
+            get { return m_MaGiaoVien; }
+            set { m_MaGiaoVien = value; }
+        }
+
+        private String m_TenGiaoVien;
+        public String TenGiaoVien
+        {
+            get { return m_TenGiaoVien; }
+            set { m_TenGiaoVien = value; }
+        }
+
+        private String m_DiaChi;
+        public String DiaChi
+        {
+            get { return m_DiaChi; }
+            set { m_DiaChi = value; }
+        }
+
+        private String m_DienThoai;
+        public String DienThoai
+        {
+            get { return m_DienThoai; }
+            set { m_DienThoai = value; }
+        }
+
+        private MonHocInfo m_MonHoc;
+        public MonHocInfo MonHoc
+        {
+            get { return m_MonHoc; }
+            set { m_MonHoc = value; }
+        }
     }
 }
